acb116799ffccc676762c31b2cb538a0b434aa4a
Rafe Taya <rafe.otaya@gmail.com>
2024-05-09 11:31:37 +0300

    Add conditionals that deal with reapting 0 or negative times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,15 @@
 const repeatString = function(string, num) {
   let text = "";
-  for (let i = 1; i <= num; i++) {
-    text += string;
+  if (num > 0) {
+    for (let i = 1; i <= num; i++) {
+      text += string;
+    }
+    return text;
+  } else if (num === 0) {
+      return "";
   }
-  return text;
+  else
+    return "ERROR";
 };
 
 // Do not edit below this line
