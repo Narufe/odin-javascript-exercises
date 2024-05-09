f68cb141488c56397649c1d0c11958e781a63d63
Rafe Taya <rafe.otaya@gmail.com>
2024-05-09 13:54:10 +0300

    Add a method that removes item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, item) {
+  let index = array.indexOf(item);
+  array.splice(index, 1);
+  return array;  
 };
 
 // Do not edit below this line
